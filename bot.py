805d409107bc476ff18ce191a737395127f22452
eric <gorskie@moravian.edu>
2022-04-19 18:27:13 -0400

    added logic for dotenv token

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
+import os
 import discord
+from dotenv import load_dotenv
 
+# Get token to give to bot
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+# Create bot
 client = discord.Client()
 
-@client.event
+@client.event 
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
@@ -14,4 +21,12 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('your token here')
+try:
+    client.run(token)
+    print('Bot is running')
+
+except discord.errors.LoginFailure as e:
+    print(e)
+    print(token)
+    print("Please check your token")
+    exit(1)
