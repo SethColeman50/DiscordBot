1401c6017d7a16d565338ae3e2793557dc19ef19
eric <gorskie@moravian.edu>
2022-04-20 09:40:06 -0400

    started adding forbidden word autodeletion

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ token = os.getenv("DISCORD_TOKEN")
 
 # Create bot
 client = discord.Client()
+client.forbidden_words = []
 
 @client.event 
 async def on_ready():
@@ -20,6 +21,27 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+    if message.content.startswith('$forbid'):
+        client.forbidden_words.append(message.content[8:])
+        await message.channel.send('Word added to forbidden words list.')
+    
+    if message.content.startswith('$delete'):
+        await message.delete()
+        await message.channel.send('Message deleted.')
+
+    if message.content.startswith('$showforbidden'):
+        if len(client.forbidden_words) == 0:
+            await message.channel.send("There are no forbidden words yet.")
+        else:
+            await message.channel.send("Forbidden words: " + ", ".join(client.forbidden_words))
+
+    for content in message.content:
+        if any(word in content for word in client.forbidden_words):
+            print("Naughty naughty!")
+            await message.delete()
+            await message.channel.send('Message deleted.')
+
 
 try:
     client.run(token)
